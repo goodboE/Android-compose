e9a17646e857322d1002e5577ca5927a7cfb3a12
HyunJin Ko <iamkokoko123@gmail.com>
2022-11-30 21:17:54 +0900

    LazyVerticalGrid

--- GridWithLazyColumn/app/src/main/java/com/example/gridwithlazycolumn/MainActivity.kt
+++ GridWithLazyColumn/app/src/main/java/com/example/gridwithlazycolumn/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.gridwithlazycolumn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.gridwithlazycolumn.data.DataSource
+import com.example.gridwithlazycolumn.model.Topic
 import com.example.gridwithlazycolumn.ui.theme.GridWithLazyColumnTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +27,86 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GridWithLazyColumnTheme {
-                // A surface container using the 'background' color from the theme
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    TopicGrid(DataSource.topics)
                 }
             }
         }
     }
 }
 
+
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TopicGrid(topicList: List<Topic>,modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier.padding(8.dp)
+    ) {
+        items(topicList) { topic ->
+            TopicCard(topic)
+        }
+    }
 }
+
+
+@Composable
+fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card(elevation = 4.dp) {
+        Row() {
+
+            Box {
+                Image(
+                    painter = painterResource(topic.imageResource),
+                    contentDescription = stringResource(topic.stringResourceId),
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(68.dp)
+                )
+            }
+
+
+            Column {
+                Text(
+                    text = stringResource(topic.stringResourceId),
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier
+                        .padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 8.dp)
+                )
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(start = 16.dp)
+                            .size(12.dp)
+                    )
+
+                    Text(
+                        text = topic.count.toString(),
+                        style = MaterialTheme.typography.caption,
+                        modifier = Modifier
+                            .padding(start = 8.dp)
+                    )
+                }
+
+            }
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     GridWithLazyColumnTheme {
-        Greeting("Android")
+        TopicGrid(DataSource.topics)
     }
 }
